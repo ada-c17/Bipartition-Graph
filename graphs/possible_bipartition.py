d9f1986296066654662581cd0d1df7fd4f204d83
Emily Schell <emilsche@amazon.com>
2022-09-27 12:29:23 -0400

    add time and space complexity

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -13,11 +13,12 @@ def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
         into the same partition.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(E) checking every edge until it identifies a cycle or has checked every edge without finding one.
+        Space Complexity: O(E) as it puts a new call with all associated data storage onto the stack for every edge it has to check.
     """
     for dog in dislikes:
         seen = []
         if not check_enemies(dislikes, dog, seen, None):
             return False
     return True
+
