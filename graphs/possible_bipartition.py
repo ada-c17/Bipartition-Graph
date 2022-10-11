2185935bfcce1381e51f08373274e115897f7c26
Joanna Dudley <jfdudleybmc@gmail.com>
2022-10-10 23:00:48 -0400

    Added solution, passing all tests

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -8,5 +8,29 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if not dislikes:
+        return True
+        
+    dogs_to_check = list(dislikes.keys())
+    if len(dislikes) == 1:
+            return True
+    first_dog = dogs_to_check[0]
+    queue = [first_dog]
+    dog_groups = {}
+    dog_groups[first_dog] = 1
+    
+    for i in range(1, len(dogs_to_check)):
+        if not queue and dogs_to_check[i] not in dog_groups:
+            queue.append(dogs_to_check[i])
+            dog_groups[dogs_to_check[i]] = 1
+        while queue:
+            current_dog = queue.pop(0)
+            for other_dog in dislikes[current_dog]:
+                if other_dog not in dog_groups:
+                    dog_groups[other_dog] = 2 if dog_groups[current_dog] == 1 else 1
+                    queue.append(other_dog)
+                if dog_groups[other_dog] == dog_groups[current_dog]:
+                    return False
+            
+    return True
 
