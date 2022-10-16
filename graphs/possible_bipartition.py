665f7f88c07474361a7e2a7a8174215ec11a67d9
soridalac <lacsorida@gmail.com>
2022-10-16 13:29:59 -0700

    pass all the tests

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,6 +1,24 @@
 # Can be used for BFS
 from collections import deque 
 
+COLORS = ["red", "green"]
+
+def dfs(dislikes, current_node, painted_graph, current_color):
+    neighbors = dislikes[current_node]
+    next_color = (current_color + 1) % len(COLORS)
+
+    for neighbor in neighbors:
+        color = painted_graph.get(neighbor)
+
+        if not color:
+            painted_graph[neighbor] = COLORS[next_color]
+            if not dfs(dislikes=dislikes, current_node=neighbor, painted_graph=painted_graph, current_color=next_color):
+                return False
+        elif color != COLORS[next_color]:
+            return False
+    
+    return True
+
 def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
@@ -8,5 +26,15 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    painted_graph = {}
+    current_color = 0
+
+    for node in dislikes.keys():
+        neighbors = dislikes[node]
+        if not painted_graph.get(node):
+            painted_graph[node] = COLORS[current_color]
+
+            if not dfs(dislikes=dislikes, current_node=node, painted_graph=painted_graph, current_color=current_color):
+                return False
+    return True
 
