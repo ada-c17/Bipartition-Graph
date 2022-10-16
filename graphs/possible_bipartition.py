edc9c3bc127c7070da95191af070337095768ec1
Morgan Buntin <mmbuntin@gmail.com>
2022-10-16 11:33:05 -0400

    passes all tests

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,6 +1,3 @@
-# Can be used for BFS
-from collections import deque 
-
 def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
@@ -8,5 +5,30 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    seen = {}
+    current_color = 1
+
+    for dog in dislikes.keys():
+        if not seen.get(dog):
+            seen[dog] = current_color
+
+            if not dfs(dislikes, dog, seen, current_color):
+                return False
+
+    return True
+
+def dfs(dislikes, current_node, seen, current_color):
+    neighbors = dislikes[current_node]
+
+    for neighbor in neighbors:
+        color = seen.get(neighbor)
+
+        if not color:
+            seen[neighbor] = -current_color
+            if not dfs(dislikes, neighbor, seen, -current_color):
+                return False
+        elif color != -current_color:
+            return False
+
+    return True
 
