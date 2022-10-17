60c9230fabd00e9d18fb08eb3dcdd412f07445d7
Sophia Wagner <sophiewagnerla@gmail.com>
2022-10-16 21:16:19 -0700

    Implement solution

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,6 +1,8 @@
 # Can be used for BFS
 from collections import deque 
 
+# source used for help with solution: https://www.geeksforgeeks.org/check-if-a-given-graph-is-bipartite-using-dfs/
+
 def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
@@ -8,5 +10,84 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    
+    if not dislikes:
+        return True
 
+    visited = {}
+    color = {}
+
+    #run a DFS starting at each key, to check if that graph is bipartite
+    for each_key in dislikes:
+
+        #make starting dicts
+        for item in dislikes:
+            visited[item] = False
+            color[item] = False
+
+        first_key = each_key
+        visited[first_key] = True
+        color[first_key] = False
+        if not isBipartite(dislikes, first_key, visited, color):
+            return False
+    
+    # if we get through all keys without finding a non-bipartite cycle, return true
+    return True
+
+def isBipartite(dislikes, item, visited, color):
+
+    for name in dislikes[item]:
+        # if the name hasn't been visited, recurse on its dislikes
+        if visited[name] == False:
+            visited[name] = True
+            color[name] = not color[item]
+            if not isBipartite(dislikes, name, visited, color):
+                return False
+        # if the name already has been visisted and had to be set to the same color, its not bipartite      
+        elif color[name] == color[item]:
+            return False
+    return True
+
+
+
+
+
+    # def dfs_helper(current, visited, graph):
+    #     if current not in visited:
+    #         visited.append(current)
+    #         for neighbor in graph[current]:
+    #             self.dfs_helper(neighbor, visited, graph)
+
+    # def dfs(self):
+    #     graph = self.adjacency_dict
+    #     if len(graph) == 0:
+    #         return []
+    #     first_item = list(graph.keys())[0]
+    #     visited = []
+    #     self.dfs_helper(first_item, visited, graph)
+    #     return visited
+
+
+    #for key in dict
+    #is there anywhere the key can go?
+    #is there anywhere the values can go?
+    #what if we get it wrong with the second key?
+
+    #check if there are three interconnected nodes
+    #do a dfs on the adjacency dict
+    #if the max value is 3
+    #if we ever visit a dog we've already visited in a DFS if the count is more than +1\
+    # 
+    # IF TWO PEOPLE WHO DISLIKE EACHOTHER BOTH DISLIKE THE SAME PERSON, WE'RE SCREWED
+    # DISLIKE MEANS WE CANT BE IN THE SAME ROOM
+    # CANT BE IN THE SAME ROOM IF: DIRECTLY DISLIKE, OR, 
+    # If we're on the same chain, it means we'd have to be in the same room?  
+
+    # for any two who dislike eachother, do you have to make another partition
+    # for any two people who dislike, if the same person comes up on both sides of DFS, we are screwed?
+
+    # if there is a cycle with odd number of nodes
+    #do a DFS on adjacency dict
+    #store the names of the visited nodes
+    #if we ever get a neighbor that's been visited, that's a cycle
+    # check length of list
