75c0adad779d54fad9ba9d8450d57bbf1c354d4e
shelby-faulconer <shelby.faulconer@disneystreaming.com>
2022-10-19 10:16:30 -0700

    pseudocode added

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,6 +1,11 @@
 # Can be used for BFS
 from collections import deque 
 
+class Graph:
+    def __init__(self, nodes : int):
+        self.adjlist = []
+        self.nodes = nodes
+
 def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
@@ -8,5 +13,21 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
+    ## input: dislikes dictionary:
+        ## dog names are key: [list of dogs that do not get along]
+    ## output: True or False
+        ## true if it is possible to split dogs into two groups
+        ## false if it is not possible
+
+    ## goal: create a graph with adjacency list
+        ## navigate the graph through traversal
+    
+    #dictionary of nodes and its edges is a graph!
+
+    #store dogs names, store relationship, and check if they can be
+        # added to a group
+    # nodes with connected edges cannot
+        # be added to to group together
+
     pass
 
