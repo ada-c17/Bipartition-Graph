6a7866354e69b067aeebb19117302da8f36a4082
Katina Carranza <katina.carranza@gmail.com>
2022-10-20 07:54:51 -0700

    Bipartition Graph Problem

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -8,5 +8,28 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+
+    if len(dislikes) == 0:
+        return False
+    first_item = list(dislikes.keys())[1]
+    stack = [first_item]
+    result = True
+    group_1 = []
+    group_2 = []
+
+    # while stack:
+    #     current = stack.pop(0)
+    #     group_1.append(current)
+    for pet in dislikes:
+        group_1.append(dislikes[pet])
+        group_2.append(pet)
+        if pet in group_1 and pet in group_2:
+            result = False
+        if pet not in group_1 and pet not in group_2:
+            result = True
+    if len (group_1) and len(group_2) == 0:
+        return False
+    
+    return result
+
 
