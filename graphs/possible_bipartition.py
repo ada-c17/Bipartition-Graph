10d199065cdc1e2254d73a4a293de5af0afdd5c2
Danielle <dmbecker24@gmail.com>
2022-10-20 08:16:38 -0700

    Implement dfs, pass all tests

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,12 +1,31 @@
 # Can be used for BFS
-from collections import deque 
+from collections import deque
+from locale import currency
+
 
 def possible_bipartition(dislikes):
-    """ Will return True or False if the given graph
-        can be bipartitioned without neighboring nodes put
-        into the same partition.
-        Time Complexity: ?
-        Space Complexity: ?
-    """
-    pass
+    color = {}
+    def dfs(current, curr_color):
+        neighbors = dislikes[current]
+        if current in color:
+            if color[current] != curr_color:
+                return False
+            else:
+                return True
+        else:
+            color[current] = curr_color
+        for each_node in dislikes[current]:
+            if not dfs(each_node, not curr_color):
+                return False
+        return True
+    
+    for node in dislikes.keys():
+        neighbors = dislikes[node]
+        if node not in color:
+            if not dfs(node, True):
+                return False
+    return True
+            
+        
+
 
