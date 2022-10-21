89950071ef5c11d4bbc0157b153be6792a2530c7
shelby-faulconer <shelby.faulconer@disneystreaming.com>
2022-10-20 21:09:00 -0700

    made a dfs helper but it is currently broken

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,8 +1,9 @@
 # Can be used for BFS
 from collections import deque 
 
+
 class Graph:
-    def __init__(self, nodes : int):
+    def __init__(self, nodes):
         self.adjlist = []
         self.nodes = nodes
 
@@ -29,5 +30,36 @@ def possible_bipartition(dislikes):
     # nodes with connected edges cannot
         # be added to to group together
 
-    pass
+## create a dictionary, keys of the dictionary are dogs names
+## values of the dictionary are dogs who fight
+    dogs_dictionary = {}
+    num_dogs = len(dislikes)
+
+# dogs names are stored in a, if dogs are elements in a
+# move them to dogs_b
+    # dogs_a = []
+    # dogs_b = []
+# if the length of either list is less than 1, return false
+    for a,b in dislikes:
+        dogs_dictionary[a].append(b)
+        dogs_dictionary[b].append(a)
+    
+    dogs_a = {i:None for i in range(num_dogs+1)}
+
+    def dfs_helper(node,dog_friends):
+        if not dogs_a[node]:
+            dogs_a[node] = dog_friends
+        else:
+            return dogs_a[node] == dog_friends
+    
+        for dogs in dogs_dictionary[node]:
+            if not dfs_helper(dogs, 2 if k == 1 else 1):
+                return False
+        return True
+    
+    for n in range(i, num_dogs + 1):
+        if not dogs_a[n] and not dfs_helper(n, 1):
+            return False
+    return True
+
 
