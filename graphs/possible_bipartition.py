5f892646647b51ac2a9c988c303c2a50da5d69e4
Megan Korling <korlingm@gmail.com>
2022-10-24 18:10:09 -0700

    working solution

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -8,5 +8,34 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if not dislikes:
+        return True
 
+    visited = {}
+    color = {}
+
+    # set up dictionaries
+    for dog_key in dislikes:
+        visited[dog_key] = False
+        color[dog_key] = False
+
+    # start checking for biparte
+    for dog_key in dislikes:
+        visited[dog_key] = True
+        if not bipartition_helper(dislikes, dog_key, visited, color):
+            return False
+    return True
+
+def bipartition_helper(dislikes, dog_key, visited, color):
+    for dog in dislikes[dog_key]:
+        # check if it's visited or not, 
+        if not visited[dog]:
+            visited[dog] = True
+            # if it's not visited, change the color to opposite of the dog_key
+            color[dog] = not color[dog_key]
+            # recurse through that dog's enemies
+            if not bipartition_helper(dislikes, dog, visited, color):
+                return False
+        elif color[dog] == color[dog_key]:
+            return False
+    return True
