c600863f3f8b49fd81e4b32b0efbb641bab28983
Liqingcathy <cathy.glc@gmail.com>
2022-10-24 22:27:04 -0700

    added working code

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,12 +1,20 @@
-# Can be used for BFS
-from collections import deque 
+def possible_bipartition(dislikes):            
 
-def possible_bipartition(dislikes):
-    """ Will return True or False if the given graph
-        can be bipartitioned without neighboring nodes put
-        into the same partition.
-        Time Complexity: ?
-        Space Complexity: ?
-    """
-    pass
-
+    def dfs(v, color):
+        if v in groups:
+            if color != groups[v]:
+                return False
+        else:
+            groups[v] = color
+            for nei in dislikes[v]:
+                if not dfs(nei, color ^ 1): #change group/color interchangebly
+                    return False
+        return True
+			
+    groups = {}
+    #tracks visited or not and which group it belongs to 
+    for v in dislikes:
+        if v not in groups:
+            if not dfs(v, 0):
+                return False
+    return True
