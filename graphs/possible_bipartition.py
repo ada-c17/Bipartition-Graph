63adf706d6fee5a5e9f9ed0ad764f0c95d3b3635
Tetiana Zakipna <tetiana.zakipna@gmail.com>
2022-10-27 10:32:27 -0700

    Two possible solutiona with que and with stack

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -8,5 +8,57 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if len(dislikes) == 0:
+        return True
 
+# solution with que
+    # group1 = []
+    # group2 = []
+    # puppies = list(dislikes.keys())
+    # first_dog = puppies[0]
+    # q = [first_dog]
+    # group1.append(first_dog)
+    # while q:
+    #     current = q.pop(0)
+    #     puppies.remove(current)
+    #     if current not in group1 and current not in group2:
+    #         group1.append(current)
+    #     for dog in dislikes[current]:
+    #         if dog not in group1 and dog not in group2:
+    #             q.append(dog)
+    #         if current in group1 and dog in group1:
+    #             return False
+    #         elif current in group1 and dog not in group2:
+    #             group2.append(dog)
+    #         elif current in group2 and dog in group2:
+    #             return False
+    #         elif current in group2 and dog not in group1:
+    #             group1.append(dog)
+    #     if len(q) == 0:
+    #         if len(puppies) != 0:
+    #             q.append(puppies[0])
+    # return True
+
+#solution with stack
+    group1 = []
+    group2 = []
+    puppies = list(dislikes.keys())
+    group1.append(puppies[-1])
+    while puppies:
+        current = puppies.pop()
+        if current not in group1 and current not in group2:
+            group1.append(current)
+        for dog in dislikes[current]:
+            if dog not in group1 and dog not in group2:
+                if dog in puppies:
+                    puppies.remove(dog)
+                    puppies.append(dog)
+            if current in group1 and dog in group1:
+                return False
+            elif current in group1 and dog not in group2:
+                group2.append(dog)
+            elif current in group2 and dog in group2:
+                return False
+            elif current in group2 and dog not in group1:
+                group1.append(dog)
+    return True
