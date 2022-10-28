1be7ae9425c5f0a6417421ad69a041d21cf24691
Marlyn <marlynlopezz@gmail.com>
2022-10-27 23:58:02 -0700

    Passing tests

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,6 +1,13 @@
 # Can be used for BFS
 from collections import deque 
 
+# dislikes = {
+#              "Fido": [],
+#              "Nala": ["Cooper", "Spot"],
+#              "Cooper": ["Nala", "Spot"],
+#              "Spot": ["Nala", "Cooper"]
+#              }
+
 def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
@@ -8,5 +15,24 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    
+
+    dog_groups = {}
+
+    # if len(dislikes) == 0:
+    #     return True
+
+    for i in dislikes:
+        if i not in dog_groups:
+            stack = [i]
+            dog_groups[i] = 0
+            while stack:
+                i = stack.pop()
+                for dog in dislikes[i]:
+                    if dog not in dog_groups:
+                        stack.append(dog)
+                        dog_groups[dog] = 1 - dog_groups[i]
+                    elif dog_groups[dog] == dog_groups[i]:
+                        return False
+    return True
 
