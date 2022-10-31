82ade20bacbd43d5a08f4ec156f0a9e6afecfad2
shelby <faulconershelby@gmail.com>
2022-10-30 19:49:12 -0700

    debug to make dfs_helper accurately assign dogs to groups and return true/ false accordingly

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,12 +1,6 @@
 # Can be used for BFS
 from collections import deque 
 
-
-# class Graph:
-#     def __init__(self, nodes):
-#         self.adjlist = []
-#         self.nodes = nodes
-
 def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
@@ -14,52 +8,33 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    ## input: dislikes dictionary:
-        ## dog names are key: [list of dogs that do not get along]
-    ## output: True or False
-        ## true if it is possible to split dogs into two groups
-        ## false if it is not possible
-
-    ## goal: create a graph with adjacency list
-        ## navigate the graph through traversal
-    
-    #dictionary of nodes and its edges is a graph!
-
-    #store dogs names, store relationship, and check if they can be
-        # added to a group
-    # nodes with connected edges cannot
-        # be added to to group together
-
-## create a dictionary, keys of the dictionary are dogs names
-## values of the dictionary are dogs who fight
-    # dogs_dictionary = {}
-    
-    # num_dogs = len(dislikes)
-
-    # for a,b in dislikes:
-    #     dogs_dictionary[a].append(b)
-    #     dogs_dictionary[b].append(a)
-    
     dogs_dictionary = {i:None for i in dislikes.keys()}
 
-    def dfs_helper(node, dog_friends):
-        if not dogs_dictionary[node]:
-            #mark the node of dog passed in search
-            dogs_dictionary[node] = dog_friends
-        # else:
-        #     return dislikes[node] == dog_friends
+    def dfs_helper(node):
     
+    # for loop assigns dog to the dog group that it will be apart of 
         for dogs in dislikes[node]:
+            # checks if dog has been assigned a group
             if dogs_dictionary[dogs]:
-                if dogs_dictionary[dogs] == dog_friends:
+                # early return if there can not be bipartisan in the group
+
+                if dogs_dictionary[node] == dogs_dictionary[dogs]:
                     return False
             else:
-                if not dfs_helper(dogs, 2 if dog_friends == 1 else 1):
+            # assigns dog to group or return false if
+            # bipartisan is not possible
+                dogs_dictionary[dogs] = 2 if dogs_dictionary[node] == 1 else 1
+                if not dfs_helper(dogs):
                     return False
         return True
-        
+    
+    # check if dog has not been assigned group
+    # assign it to group 1
+    # return false if dfs helper returns false    
     for dog in dislikes.keys():
-        if not dislikes[dog] and not dfs_helper(dog, 1):
+        if not dogs_dictionary[dog]:
+            dogs_dictionary[dog] = 1
+        if not dfs_helper(dog):
             return False
     return True
 
