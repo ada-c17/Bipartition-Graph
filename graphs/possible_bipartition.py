c7d6652452bb1977faeb9612fdb66bfd8e40ab45
Natascha <njenkns@gmail.com>
2022-11-01 05:58:56 -0700

    passes all tests

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,6 +1,3 @@
-# Can be used for BFS
-from collections import deque 
-
 def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
@@ -8,5 +5,23 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    dogs = {}
+
+    def dfs(dog):
+        for sad_dog in dislikes[dog]:
+            if sad_dog in dogs: 
+                if dogs[sad_dog] == dogs[dog]:
+                    return False
+            else:
+                dogs[sad_dog] = 1 - dogs[dog]
+                if not dfs(sad_dog):
+                    return False
+        return True
+    
+    for dog in dislikes.keys():
+        if dog not in dogs:
+            dogs[dog] = 0
+            if not dfs(dog):
+                return False
+    return True 
 
