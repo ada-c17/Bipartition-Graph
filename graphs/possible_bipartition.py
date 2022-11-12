fcafe11fcbb64256dfc0943e3b8381dfc311e75a
Danielle <dwhyte1226@gmail.com>
2022-11-12 15:09:57 -0600

    bipartite graph solution

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -8,5 +8,35 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if not dislikes:
+        return True
+    first = list(dislikes.keys())[0]
+    all_nodes = set(list(dislikes.keys()))
+    stack = []
+    group1 = set()
+    group2 = set()
+    visited = set()
+    stack.append(first)
+    group1.add(first)
+
+    while stack: 
+        current = stack.pop(0)
+        visited.add(current)
+        current_group = group1 if current in group1 else group2
+        other_group = group2 if current_group is group1 else group1
+        for neighbor in dislikes[current]: 
+            if neighbor not in visited:
+                stack.append(neighbor)
+            if neighbor in current_group:
+                return False
+            other_group.add(neighbor)
+
+        #handle disconnected graphs
+        if (not stack) and (len(visited) != len(all_nodes)):
+            for node in all_nodes:
+                if node not in visited:
+                    stack.append(node)
+                    print(stack)
+                    break
+    return True
 
