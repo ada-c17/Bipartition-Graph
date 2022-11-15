fe518deaae095bbe6f0acfd15e14568c9c07ffbb
Taingleap Chheang <chheangtaingleap@gmail.com>
2022-11-14 16:08:08 -0800

    added solution and passed all test cases

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -8,5 +8,23 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    color = {key: -1 for key in dislikes}
 
+    def is_bipartite(key):
+        color[key] = 1
+        queue = deque([key])
+        while queue:
+            node = queue.popleft()
+            for hater in dislikes[node]:
+                if color[hater] == color[node]:
+                    return False
+                if color[hater] == -1:
+                    color[hater] = 1 - color[node]
+                    queue.append(hater)
+        return True
+
+    for key in dislikes:
+        if color[key] == -1:
+            if not is_bipartite(key):
+                return False
+    return True 
