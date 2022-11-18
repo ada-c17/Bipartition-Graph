9c2c46a87be5ad6c8b726491ed874f8fdca07eee
Natalia <natalia.woodson@gmail.com>
2022-11-17 20:17:47 -0500

    add solution

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -9,12 +9,24 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    if not dislikes:
-        return True
-
-    first_dog = list(dislikes.keys())[0]
-
-    groups = {}
+    group = {}
+    stack = []
+    
+    for dog in dislikes:
+        if dog not in group: 
+            stack.append(dog) 
+            group[dog] = 0
+            while stack:
+                current_dog = stack.pop() 
+                for neighbor in dislikes[current_dog]:
+                    if neighbor not in group: 
+                        stack.append(neighbor)
+                        group[neighbor] = 1 - group[current_dog] 
+                    elif group[neighbor] == group[dog]:
+                        return False
+    
+    return True
+    
 
 
 
