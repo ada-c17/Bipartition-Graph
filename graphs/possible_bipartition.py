74de0f377849ec9d649c0db6376644ff52130dfa
olive <olivealavine@gmail.com>
2022-11-19 15:15:50 -0600

    tests passing

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,12 +1,24 @@
 # Can be used for BFS
 from collections import deque 
 
-def possible_bipartition(dislikes):
-    """ Will return True or False if the given graph
-        can be bipartitioned without neighboring nodes put
-        into the same partition.
-        Time Complexity: ?
-        Space Complexity: ?
-    """
-    pass
 
+def possible_bipartition(dislikes):
+    if not dislikes or len(dislikes) <= 2:
+        return True
+
+    visited = {}
+    for dog in dislikes.keys():
+        visited[dog] = False
+    
+    for dog in visited.keys():
+        queue = [dog]
+        while queue:
+            dog1 = queue.pop()
+            for dog2 in dislikes[dog1]:
+                if not visited[dog2]:
+                    visited[dog2] = 'blue' if (visited[dog1] == 'red') else 'red'
+                    queue.append(dog2)
+                else:
+                    if visited[dog1] == visited[dog2]:
+                        return False
+    return True
