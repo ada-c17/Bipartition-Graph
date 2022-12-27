debb5d5eb899f8ef5b5ad44953fe2e2ef335898b
Vida Maleki <vida.basmani@gmail.com>
2022-12-26 21:26:22 -0800

    passed all 10 tests and finished project

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -8,5 +8,34 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if not dislikes: 
+        return True
 
+    groups = {}
+    
+    for key in dislikes.keys():
+        groups[key] = 0
+    
+    first_dog = list(dislikes.keys())[0]
+
+    groups[first_dog] = 1
+    queue = [first_dog]
+    visited = []
+
+    for dog in dislikes: 
+        while queue:
+            current_i = queue.pop(0)
+            visited.append(current_i)
+
+            if dislikes[current_i]:
+                for frenemie in dislikes[current_i]:
+                    if groups[frenemie] == 0:
+                        groups[frenemie] = groups[current_i] + 1
+                        queue.append(frenemie)
+                    else:
+                        if groups[frenemie] == groups[current_i]:
+                            return False
+            
+        if dog not in visited:
+            queue.append(dog)
+    return True
