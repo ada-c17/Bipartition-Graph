b062da48d0358f146b5cc23394e81feea3d22b52
HaileyMatz <hmatz525@gmail.com>
2023-01-09 14:14:23 -0800

    Pass all the tests

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,12 +1,35 @@
 # Can be used for BFS
 from collections import deque 
 
-def possible_bipartition(dislikes):
-    """ Will return True or False if the given graph
-        can be bipartitioned without neighboring nodes put
-        into the same partition.
-        Time Complexity: ?
-        Space Complexity: ?
-    """
-    pass
+def start_new_queue(dislikes, visited):
+    first_item = list(dislikes.keys())[0]
+    visited[first_item] = 0
+    return [first_item]
 
+def possible_bipartition(dislikes):
+
+    # BFS solution
+    if len(dislikes) == 0:
+        return True
+
+    visited = {}
+    queue = start_new_queue(dislikes, visited)
+
+    while queue:
+        current = queue.pop(0)
+        curr_color = visited[current]
+        for neighbor in dislikes[current]:
+            if neighbor not in visited:
+                visited[neighbor] = 1 if curr_color == 0 else 0
+                queue.append(neighbor)
+            else:
+                if curr_color == visited[neighbor]:
+                    return False
+        if len(queue) == 0 and len(visited) < len(dislikes):
+            new_dislikes = {}
+            for key, value in dislikes.items():
+                if key not in visited:
+                    new_dislikes[key] = value
+            dislikes = new_dislikes
+            queue = start_new_queue(dislikes, visited)
+    return True
