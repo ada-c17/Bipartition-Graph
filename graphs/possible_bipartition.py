bc428decc00e111b88557d973bf5e626ebfa549b
Diana <arantxa.codes@gmail.com>
2023-01-16 12:17:42 -0800

    Adding funcs to test bipartition

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -8,5 +8,29 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if not dislikes:
+            return True
 
+    visited = {}
+    color = {}
+
+    for dog_name in dislikes:
+        visited[dog_name] = False
+        color[dog_name] = False
+            
+    for dog_name in dislikes:
+        visited[dog_name] = True
+        if not helper_func(dislikes, dog_name, visited,color):
+            return False
+    return True
+
+def helper_func(dislikes, dog_name, visited, color):
+    for dog in dislikes[dog_name]:
+        if not visited[dog]:
+            visited[dog] = True
+            color[dog] = not color[dog_name]
+            if not helper_func(dislikes, dog, visited, color):
+                return False
+        elif color[dog] == color[dog_name]:
+            return False
+    return True
