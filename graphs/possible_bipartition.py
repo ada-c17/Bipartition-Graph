45046e991a7b58ea3e89d8a3b5f66c33b3703695
camilla <pasadenastringtheory@gmail.com>
2023-01-20 18:17:02 -0800

    Implements solution

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -5,9 +5,22 @@ def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
         into the same partition.
-        Time Complexity: ?
-        Space Complexity: ?
-        HOWDY, STRANGER!
+        Time Complexity: O(N + E)
+        Space Complexity: O(N)
     """
-    pass
+    visited = {}
+
+    def dfs(dog, group):
+        if dog in visited:
+            return visited[dog] == group
+        visited[dog] = group
+        for neighbor in dislikes.get(dog, []):
+            if not dfs(neighbor, not group):
+                return False
+        return True
+
+    for dog in dislikes:
+        if dog not in visited and not dfs(dog, True):
+            return False
+    return True
 
